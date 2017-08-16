f473a89be4ebdee1cf4d56d805f9d1bec0388ffd
JmoVxia <269968846@qq.com>
2017-08-16 14:38:13 +0800

    新建工程

---
+++ CLInputToolbar/CLInputToolbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLInputToolbar
+//
+//  Created by JmoVxia on 2017/8/16.
+//  Copyright © 2017年 JmoVxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CLInputToolbar/CLInputToolbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CLInputToolbar
+//
+//  Created by JmoVxia on 2017/8/16.
+//  Copyright © 2017年 JmoVxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
